093fb1e0ac859113006e374e0b7d4ca9f3fbcab9
Craig Banach <craigsb93@gmail.com>
2021-10-31 15:03:12 +0000

    Replace ethers with Web3 & first contract function

--- contracts/CouncilProjectFunding.sol
+++ contracts/CouncilProjectFunding.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.9;
 
 contract CouncilProjectFunding {
   
+  mapping(uint => Project) public projects;
+  uint public projectCount;
+
   struct Project {
     uint id;
     string name;
@@ -10,10 +13,42 @@ contract CouncilProjectFunding {
     uint fundingNeeded;
     uint currentFundLevel;
     uint thresholdBlock;
-    mapping (address => uint) funders;
   }
 
-  mapping (uint => Project) public projects;
+  modifier stringNotEmpty(string calldata str) {
+    require(bytes(str).length != 0);
+    _;
+  }
+
+  modifier uintNotZero(uint num) {
+    require(num != 0);
+    _;
+  }
+
+  function addProject(
+    string calldata name,
+    string calldata description,
+    uint fundingNeeded,
+    uint thresholdBlock
+  ) public 
+    stringNotEmpty(name) 
+    stringNotEmpty(description) 
+    uintNotZero(fundingNeeded)
+    uintNotZero(thresholdBlock) 
+    returns (bool success) 
+  {
+    projects[projectCount] = Project(
+      projectCount,
+      name,
+      description,
+      fundingNeeded,
+      0,
+      thresholdBlock
+    );
+    projectCount++;
+    return true;
+  }
+
 
   function fundProject(uint projectId) public payable returns (bool success) {
 
--- front-end/src/pages/Projects.tsx
+++ front-end/src/pages/Projects.tsx
@@ -1,35 +1,77 @@
-import { useWeb3React } from '@web3-react/core';
-import { InjectedConnector } from '@web3-react/injected-connector'
-const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42],
-})
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import CPF_ABI from '../abis';
 
 function Projects() {
-    const { active, account, library, connector, activate, deactivate } = useWeb3React();
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState<Array<any>>();
 
-    async function connect() {
-        try {
-          await activate(injected)
-        } catch (ex) {
-          console.log(ex)
-        }
-      }
+  useEffect(() => {
+    loadBlockchainData();
+  }, [])
 
-      async function disconnect() {
-        try {
-          deactivate()
-        } catch (ex) {
-          console.log(ex)
-        }
-      }
+  async function loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const accounts = await web3.eth.getAccounts();
+    const cpf = new web3.eth.Contract(CPF_ABI as any, "0x9f148817fd7060C898EAA19ef758Ca9a33902759");
+    console.log(await cpf.methods.projectCount().call());
+    const projects = await cpf.methods.projects(0).call()
+    console.log(projects);
+    setProjects([projects, projects]);
+    setLoading(false);
+  }
 
-      return (
-        <div className="flex flex-col items-center justify-center">
-          <button onClick={connect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Connect to MetaMask</button>
-          {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
-          <button onClick={disconnect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect</button>
-        </div>
-      )
+  // const [cpf, setCpf] = useState(new ethers.Contract(
+  //   "0x9f148817fd7060C898EAA19ef758Ca9a33902759",
+  //   CPF_ABI)
+  // );
+  // //const [projectCount, setProjectCount] = useState();
+
+  // async function getProjectCount() {
+  //   try {
+  //     const data = await cpf.projectCount().call();
+  //     console.log('data: ', data);
+  //   } catch (err) {
+  //     console.log("Error: ", err)
+  //   }
+  // }
+
+  async function connect() {
+    try {
+      // await activate(injected)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  async function disconnect() {
+    try {
+      // deactivate()
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  let projectList = projects != null && projects.length > 0
+    ? projects.map((project: { id: number }) => {
+      return (<li key={project.id}>{project.id}</li>)
+    })
+    : null;
+
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center">
+        Loading
+      </div>
+    )
+  }
+  return (
+    <div className="d-flex justify-content-center">
+      <ul>
+        {projectList}
+      </ul>
+    </div>
+  )
 }
 
 export default Projects;
